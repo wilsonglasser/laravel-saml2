d2beb45c81cbdc09052466ae8195b8e97849dc11
Wilson Glasser <wilsonglasser@gmail.com>
2020-03-11 12:58:41 -0300

    fixed for laravel 5.1

--- src/routes.php
+++ src/routes.php
@@ -4,32 +4,30 @@ Route::group([
     'prefix' => config('saml2_settings.routesPrefix'),
     'middleware' => config('saml2_settings.routesMiddleware'),
 ], function () {
-    Route::prefix('{idpName}')->group(function() {
-	$saml2_controller = config('saml2_settings.saml2_controller', 'Aacotroneo\Saml2\Http\Controllers\Saml2Controller');
+    $saml2_controller = config('saml2_settings.saml2_controller', 'Aacotroneo\Saml2\Http\Controllers\Saml2Controller');
 
-        Route::get('/logout', array(
-            'as' => 'saml2_logout',
-            'uses' => $saml2_controller.'@logout',
-        ));
+    Route::get('/logout', array(
+        'as' => 'saml2_logout',
+        'uses' => $saml2_controller . '@logout',
+    ));
 
-        Route::get('/login', array(
-            'as' => 'saml2_login',
-            'uses' => $saml2_controller.'@login',
-        ));
+    Route::get('/login', array(
+        'as' => 'saml2_login',
+        'uses' => $saml2_controller . '@login',
+    ));
 
-        Route::get('/metadata', array(
-            'as' => 'saml2_metadata',
-            'uses' => $saml2_controller.'@metadata',
-        ));
+    Route::get('/metadata', array(
+        'as' => 'saml2_metadata',
+        'uses' => $saml2_controller . '@metadata',
+    ));
 
-        Route::post('/acs', array(
-            'as' => 'saml2_acs',
-            'uses' => $saml2_controller.'@acs',
-        ));
+    Route::post('/acs', array(
+        'as' => 'saml2_acs',
+        'uses' => $saml2_controller . '@acs',
+    ));
 
-        Route::get('/sls', array(
-            'as' => 'saml2_sls',
-            'uses' => $saml2_controller.'@sls',
-        ));
-    });
+    Route::get('/sls', array(
+        'as' => 'saml2_sls',
+        'uses' => $saml2_controller . '@sls',
+    ));
 });
